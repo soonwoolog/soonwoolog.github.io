adb4c04ed224a870c2d741640fc62c9c3ce33834
soonwoolog <tnsdn94560@gmail.com>
2025-07-07 12:06:52 -0400

    Move to src folder

--- app/api/notion/[pageId]/route.ts
+++
@@ -1,8 +0,0 @@
-import { getContent } from "@/lib/notion";
-import { NextResponse } from "next/server";
-
-export async function GET(_: Request, context: { params: { pageId: string } }) {
-  const pageId = context.params.pageId;
-  const posts = await getContent(pageId);
-  return NextResponse.json(posts);
-}
--- app/api/notion/route.ts
+++
@@ -1,7 +0,0 @@
-import { getPosts } from "@/lib/notion";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const posts = await getPosts();
-  return NextResponse.json(posts);
-}
