2db37d40f71f58922e1767708956271ae02f940b
soonwoolog <tnsdn94560@gmail.com>
2025-07-25 23:04:26 -0400

    Update NotionPostPage

--- src/app/post/[...slug]/page.tsx
+++ src/app/post/[...slug]/page.tsx
@@ -1,7 +1,8 @@
 import { getNotionPageBySlug } from "nextjs-notion-integration";
 import NotionPage from "components/notion/page/NotionPage";
 
-export default async function NotionPostPage({ params }: { params: { slug: string[] } }) {
+export default async function NotionPostPage(props: { params: Promise<{ slug: string[] }> }) {
+  const params = await props.params;
   const data = await getNotionPageBySlug(params.slug);
   return <NotionPage blocks={data} />;
 }
